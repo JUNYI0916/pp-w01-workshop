f3402c986e12d21925892f2b25d325fc30f40b24
JUNYI0916 <junyi.h0916@gmail.com>
2022-10-06 23:16:16 +0100

    new text for py

--- new_test.py
+++ new_test.py
@@ -1,1 +1,5 @@
-
+name = 'kwanngai'
+age = 25
+birthday = 'Sep.16th'
+'''state = 'my name is ', name, 'on ', birthday, ' i will be ', age + 1, 'years old'''
+print('my name is ', name, 'on ', birthday, ' i will be ', age + 1, 'years old')
